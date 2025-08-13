fea1e6b24e12de7b427c194e31141af03d3791d9
enxpower <info@energizeos.com>
2025-08-13 12:51:00 -0700

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,3 @@
+const footer = document.createElement("footer");
+footer.innerHTML = "<p style='text-align:center;color:#555;'>© 2025 Energy News Aggregator</p>";
+document.body.appendChild(footer);
